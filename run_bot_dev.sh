263c1dbf5e44b1a0d9030f0a65619a39de65c724
Alexander <hazadus7@gmail.com>
2022-10-04 22:19:54 +0300

    Delete run_bot_dev.sh

--- run_bot_dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Set these env vars according to yours
-export BOT_TOKEN=5532455951:AAEDc794hoSqelOSBNaVUr_FoSI5j6Kilmg
-export BOT_ADMIN=133637887
-export BOT_LOG_FILENAME=aiogram_bot.log
-#export BOT_SQLITE_FILENAME=aiogram_bot.db
-
-python ./aiogram-youbot.py
